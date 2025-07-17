82ae93c1e7c76ab1fe2a8284339b3892845c33cd
diego <diegoalvesdc12@gmail.com>
2025-07-17 14:59:42 -0300

    ✨ :: [Calendar][Front] :: Implementa página de calendário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 
+import { OnInit } from '@angular/core';
+import { PrimeNG } from 'primeng/config';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +14,10 @@ import { HeaderComponent } from "./layout/header/header.component";
 })
 export class AppComponent {
   title = 'full_lap';
+  
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit() {
+      this.primeng.ripple.set(true);
+  }
 }
--- src/app/features/calendar/calendar.component.ts
+++ src/app/features/calendar/calendar.component.ts
@@ -1,20 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
 import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 
+interface Evento {
+  titulo: string;
+  categoria: string;
+  local: string;
+  data: string;
+  cor: string;
+  circuit: Circuit;
+}
+
+interface Periodo {
+  intervalo: string;
+  eventos: Evento[];
+}
+
+interface Circuit {
+  path: string;
+}
 
 @Component({
   selector: 'app-calendar',
-  imports: [FullCalendarModule],
+  imports: [FullCalendarModule, TimelineModule, CardModule, CommonModule],
   standalone: true,
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+
+
+eventosPorPeriodo: Periodo[] = [
+  {
+    intervalo: '04-06 de Julho',
+    eventos: [
+      {
+        titulo: 'GP da Grã-Bretanha',
+        categoria: 'F1',
+        local: 'Silverstone',
+        data: '06/07/2025 11:00',
+        cor: '#EE6F17',
+        circuit: {
+          path: 'formula1/ferrari-2.jpg'
+        }
+      },
+      {
+        titulo: '6 Horas de Monza',
+        categoria: 'WEC',
+        local: 'Monza',
+        data: '06/07/2025 07:00',
+        cor: '#E21A30',
+        circuit: {
+          path: 'formula-indy/formula-indy-1.jpg'
+        }
+      },
+      {
+        titulo: 'Etapa de Cascavel',
+        categoria: 'Nascar Brasil',
+        local: 'Cascavel',
+        data: '06/07/2025 14:00',
+        cor: '#E21A30',
+        circuit: {
+          path: 'stock-car/stock-car-1.jpg'
+        }
+      }
+    ]
+  },
+  {
+    intervalo: '25-27 de Julho',
+    eventos: [
+      {
+        titulo: 'Etapa do Veloccita',
+        categoria: 'Porsche Cup',
+        local: 'Velocitta',
+        data: '26/07/2025 15:00',
+        cor: '#009639',
+        circuit: {
+          path: 'porsche-cup/porsche-cup-nao-usar.jpg'
+        }
+      }
+    ]
+  },
+  {
+    intervalo: '14-15 de Julho',
+    eventos: [
+      {
+        titulo: 'Etapa Interlagos',
+        categoria: 'Nascar Brasil',
+        local: 'Interlagos',
+        data: '15/07/2025 10:00',
+        cor: '#009639',
+        circuit: {
+          path: 'formula1/ferrari.jpg'
+        }
+      }
+    ]
+  }
+];
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
