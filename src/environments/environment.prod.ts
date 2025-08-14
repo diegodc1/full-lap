ad13a8bc565e855279c69389670538d303198811
diego.costa <diego.costa@rpinfo.com.br>
2025-08-14 11:39:38 -0300

    🔧 :: [Env] :: Configura arquivos para variáveis de ambiente 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://visual-klarrisa-fullap-76a3b552.koyeb.app/api/'
+  apiUrl: 'https://visual-klarrisa-fullap-76a3b552.koyeb.app/api'
 };
