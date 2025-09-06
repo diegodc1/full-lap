ed31ebdcd1d61ba973040748c525b521eda4dcf1
diego <diegoalvesdc12@gmail.com>
2025-09-06 17:55:42 -0300

    🩹 :: [SEO] :: Melhora as tags de SEO

--- src/app/shared/pipes/category-image.pipe.ts
+++ src/app/shared/pipes/category-image.pipe.ts
@@ -12,11 +12,15 @@ export class CategoryImagePipe implements PipeTransform {
         case 'STOCK_CAR':
             return 'stock-car/stock-car-1.jpg';
         case 'FORMULA_INDY':
-         return 'formula-indy/formula-indy-1.jpg';
+            return 'formula-indy/formula-indy-1.jpg';        
         case 'NASCAR_BRASIL':
-          return 'nascar/nascar.jpg';
+            return 'nascar/nascar.jpg';
         case 'PORSCHE_CUP':
-          return 'porsche-cup/porsche-cup.jpg';
+            return 'porsche-cup/porsche-cup.jpg';
+        case 'COPA_TRUCK':
+            return 'copa-truck/copa-truck.jpg';
+        case 'DRIFT':
+            return 'cdrift/drift.jpg';
         default:
             return'default.jpg';
     }
